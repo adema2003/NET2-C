ff8ef73b6af65c9d7d8fbb3dfe8b1bb7e42fdb91
Thomas <thomasadema2003@gmail.com>
2021-11-28 14:51:56 +0100

    Opdracht 2b gemaakt en Word document ingevuld en geupload

---
+++ 2b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void print_function( char letter );
+int ctr;
+int main( void )
+{
+    char star = '*';
+    char dash = '-';
+    for (ctr = 0 ; ctr < 5; ctr++ )
+    {
+        print_function( star );
+        print_function( dash );
+        printf("\n");
+    }
+    return 0;
+}
+void print_function ( char letter )
+{
+    for ( ctr = 0; ctr < 5; ctr++) 
+    {
+        printf("%c", letter);
+        printf("\n");
+    }
+}
