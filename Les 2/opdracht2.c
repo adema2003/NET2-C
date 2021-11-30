0284097947b2e994e7585f28a08fa0f3b87f28cf
Thomas <thomasadema2003@gmail.com>
2021-11-30 13:25:46 +0100

    les 2 opdracht 2 gemaakt

--- Les 2/opdracht2.c
+++ Les 2/opdracht2.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
+void berekenOmtrek(int diameter, float *omtrek);
+
 int main( void ) {
+    float omtrek; 
     int diameter;
     printf("Wat is de diameter van de cirkel? ");
     scanf("%d", &diameter);  
-    printf("Dit is de diameter, %d ",diameter);
+    printf("Dit is de diameter, %d \n", diameter);
+
+    berekenOmtrek(diameter, &omtrek);
+
+    printf("Dit is de omtrek, %f \n", omtrek);
+
     return 0;
+}
+
+void berekenOmtrek(int diameter, float *omtrek)
+{
+    *omtrek = diameter * 3.14;
 }
